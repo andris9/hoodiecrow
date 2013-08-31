6c0447bfb760b72d96fff783e2741c08bc8074ca
Andris Reinman <andris@kreata.ee>
2013-08-31 12:11:00 +0300

    updated tests

--- test/toybird.js
+++ test/toybird.js
@@ -1,32 +1,34 @@
 var mockClient = require("../lib/mock-client"),
     toybird = require("../lib/server");
 
-module.exports["Example test"] = function(test){
-    var server = toybird({
-        enabled: ["ID", "IDLE", "STARTTLS", "LOGINDISABLED"],
-        separator: "/",
-        directories: {
-            "INBOX": {
-                messages:[
-                    {body: ""},
-                    {body: ""},
-                    {body: ""}
-                ]
+module.exports["Basic tests"] = {
+    setUp: function(done){
+        this.server = toybird({
+            enabled: ["ID", "IDLE", "STARTTLS", "LOGINDISABLED"],
+            separator: "/",
+            directories: {
+                "INBOX": {
+                    messages:[{}, {}, {}]
+                }
             }
-        }
-    });
-    server.addUser("testuser", "testpass");
-    server.listen(1234, function(){
-        var cmds = ["A1 CAPABILITY", 
-                "A2 STARTTLS", 
-                "A3 LOGIN testuser testpass", 
-                "A4 SELECT INBOX", 
-                "A5 SEARCH ALL"];
-        mockClient(1234, "localhost", cmds, false, function(resp){
-            test.ok(resp.toString("utf-8").match(/^\* SEARCH 1 2 3$/m));
-            server.close(function(){
-                test.done();
-            });
         });
-    });
+        this.server.addUser("testuser", "testpass");
+        this.server.listen(1234, done);
+    },
+
+    tearDown: function(done){
+        this.server.close(done);
+    },
+
+    basic: function(test){
+        var cmds = ["A1 CAPABILITY", 
+                "A2 STARTTLS",
+                "A3 LOGIN testuser testpass", 
+                "A4 SELECT INBOX",
+                "A6 LOGOUT"];
+        mockClient(1234, "localhost", cmds, false, function(resp){
+            test.ok(resp.toString("utf-8").indexOf(" OK [READ-WRITE]"));
+            test.done();
+        });
+    }
 }
