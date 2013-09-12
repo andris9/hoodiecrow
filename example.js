e938a8614b99fb78473f25108f5ee30f59da4364
Andris Reinman <andris@kreata.ee>
2013-09-12 17:16:29 +0300

    Added NAMESPACE command

--- example.js
+++ example.js
@@ -1,9 +1,33 @@
 var toybird = require("./lib/server"),
     server = toybird({
-        plugins: ["ID", "STARTTLS", "LOGINDISABLED", "AUTH-PLAIN"],
+        plugins: ["ID", "STARTTLS"/*, "LOGINDISABLED"*/, "AUTH-PLAIN", "NAMESPACE"],
         id:{
             "name": "toybird",
             "version": "0.1"
+        },
+        namespace:{
+            "":{
+                folders: {
+                    "INBOX":{
+                        messages: [
+                            {
+                                raw: "Subject: hello\r\n\r\nWorld!"
+                            }
+                        ]
+                    }
+                }
+            },
+            "#news":{
+                type: "shared",
+                separator: ".",
+                folders: {
+                    "world":{}
+                }
+            },
+            "#juke":{
+                type: "shared",
+                separator: "?"
+            }
         }
     });
 
@@ -12,3 +36,15 @@ const PORT = 1234;
 server.listen(PORT, function(){
     console.log("Toybird listening on port %s", PORT)
 });
+
+var c = 1;
+
+setInterval(function(){
+    server.notify({
+        tag: "*",
+        attributes: [
+            c++,
+            {type: "ATOM", value: "EXPUNGE"}
+        ]
+    });
+}, 5000);
