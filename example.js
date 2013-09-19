5ad5bd0ba0b9338ce34186a5883d47ddd0af147c
Andris Reinman <andris@kreata.ee>
2013-09-19 10:51:21 +0300

    Added SPECIAL-USE plugin (but no CREATE-SPECIAL-USE yet)

--- example.js
+++ example.js
@@ -1,41 +1,81 @@
 var hoodiecrow = require("./lib/server"),
     server = hoodiecrow({
-        plugins: ["ID", "STARTTLS"/*, "LOGINDISABLED"*/, "SASL-IR", "AUTH-PLAIN", "NAMESPACE", "IDLE", "ENABLE", "XTOYBIRD", "LITERALPLUS", "UNSELECT"],
+        plugins: ["ID", "STARTTLS"/*, "LOGINDISABLED"*/, "SASL-IR", "AUTH-PLAIN", "NAMESPACE", "IDLE", "ENABLE", "XTOYBIRD", "LITERALPLUS", "UNSELECT", "SPECIAL-USE"],
         id:{
             name: "hoodiecrow",
             version: "0.1"
         },
         storage: {
-            "":{
-                folders: {
-                    "INBOX":{
-                        messages: [
-                            "Subject: hello 1\r\n\r\nWorld 1!",
-                            {raw: "Subject: hello 2\r\n\r\nWorld 2!", internaldate: "14-Sep-2013 21:22:28 -0300"},
-                            {raw: "Subject: hello 3\r\n\r\nWorld 3!", flags: ["\\Seen", "\\Deleted"]},
-                            {raw: "Subject: hello 4\r\n\r\nWorld 4!", flags: "\\Answered"},
-                            "Subject: hello 5\r\n\r\nWorld 5!",
-                            "Subject: hello 6\r\n\r\nWorld 6!"
+            "": {
+                "folders": {
+                    "INBOX": {
+                        "special-use": "\\Inbox",
+                        "messages": [
+                            {
+                                "raw": "Subject: hello 1\r\n\r\nWorld 1!",
+                                "internaldate": "18-Sep-2013 17:28:15 +0300"
+                            },
+                            {
+                                "raw": "Subject: hello 2\r\n\r\nWorld 2!",
+                                "internaldate": "14-Sep-2013 21:22:28 -0300"
+                            },
+                            {
+                                "raw": "Subject: hello 4\r\n\r\nWorld 4!",
+                                "flags": [
+                                    "\\Answered"
+                                ],
+                                "internaldate": "18-Sep-2013 17:28:15 +0300"
+                            },
+                            {
+                                "raw": "Subject: hello 5\r\n\r\nWorld 5!",
+                                "internaldate": "18-Sep-2013 17:28:15 +0300"
+                            }
                         ]
+                    },
+                    "Drafts": {
+                        "special-use": "\\Drafts",
+                        "messages": []
+                    },
+                    "Test folder": {}
+                },
+                "separator": "/",
+                "type": "personal"
+            },
+            "#news.": {
+                "type": "shared",
+                "separator": ".",
+                "folders": {
+                    "world": {
+                        "path": "#news.world",
+                        "namespace": "#news.",
+                        "uid": 1,
+                        "uidvalidity": 1,
+                        "flags": [
+                            "\\HasNoChildren"
+                        ],
+                        "allowPermanentFlags": true,
+                        "permanentFlags": [
+                            "\\Answered",
+                            "\\Flagged",
+                            "\\Draft",
+                            "\\Deleted",
+                            "\\Seen"
+                        ],
+                        "subscribed": true,
+                        "messages": [],
+                        "uidnext": 1
                     }
                 }
             },
-            "#news.":{
-                type: "shared",
-                separator: ".",
-                folders: {
-                    "world":{}
-                }
-            },
-            "#juke?":{
-                type: "shared",
-                separator: "?"
+            "#juke?": {
+                "type": "shared",
+                "separator": "?"
             }
         },
         debug: true
     });
 
-const PORT = 1234;
+const PORT = 1143;
 
 server.listen(PORT, function(){
     console.log("Hoodiecrow listening on port %s", PORT)
---
+++ tests/special-use.js
@@ -0,0 +1,223 @@
+var hoodiecrow = require("../lib/server"),
+    mockClient = require("../lib/mock-client");
+
+var IMAP_PORT = 4143,
+    instance = 0;
+
+module.exports["Special-use"] = {
+    setUp: function(done){
+        this.server = hoodiecrow({
+            plugins: ["SPECIAL-USE"],
+            id:{
+                name: "hoodiecrow",
+                version: "0.1"
+            },
+            storage:{
+                "":{
+                    folders: {
+                        "INBOX":{
+                            "special-use": "\\Inbox",
+                            messages: []
+                        },
+                        "Test": {
+                            subscribed: false
+                        },
+                        "Sent mail": {
+                            "special-use": ["\\Sent", "\\Drafts"],
+                            subscribed: false
+                        }
+                    }
+                },
+                "#news.":{
+                    type: "shared",
+                    separator: ".",
+                    folders: {
+                        "world":{}
+                    }
+                },
+                "#juke?":{
+                    type: "shared",
+                    separator: "?"
+                }
+            }
+        });
+
+        this.instanceId = ++instance;
+        this.server.listen(IMAP_PORT, (function(){
+            done();
+        }).bind(this));
+    },
+
+    tearDown: function(done){
+        this.server.close((function(){
+            done();
+        }).bind(this));
+    },
+
+    "LIST NORMAL": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST \"\" \"*\"",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.equal((resp.match(/^\* LIST\b/mg) || []).length, 3);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren) "/" "INBOX"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren) "/" "Test"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren \\Sent \\Drafts) "/" "Sent mail"\r\n') >= 0);
+            test.ok(resp.indexOf("\nA3 OK") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST (SPECIAL-USE) \"\" \"*\"",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.equal((resp.match(/^\* LIST\b/mg) || []).length, 1);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren \\Sent \\Drafts) "/" "Sent mail"\r\n') >= 0);
+            test.ok(resp.indexOf("\nA3 OK") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST RETURN (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST \"\" \"*\" RETURN (SPECIAL-USE)",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.equal((resp.match(/^\* LIST\b/mg) || []).length, 3);
+            test.ok(resp.indexOf('\n* LIST () "/" "INBOX"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST () "/" "Test"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST (\\Sent \\Drafts) "/" "Sent mail"\r\n') >= 0);
+            test.ok(resp.indexOf("\nA3 OK") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST (SPECIAL-USE) RETURN (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST (SPECIAL-USE) \"\" \"*\" RETURN (SPECIAL-USE)",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.equal((resp.match(/^\* LIST\b/mg) || []).length, 1);
+            test.ok(resp.indexOf('\n* LIST (\\Sent \\Drafts) "/" "Sent mail"\r\n') >= 0);
+            test.ok(resp.indexOf("\nA3 OK") >= 0);
+            test.done();
+        }).bind(this));
+    }
+};
+
+
+module.exports["No Special-use"] = {
+    setUp: function(done){
+        this.server = hoodiecrow({
+            storage:{
+                "":{
+                    folders: {
+                        "INBOX":{
+                            "special-use": "\\Inbox",
+                            messages: []
+                        },
+                        "Test": {
+                            subscribed: false
+                        },
+                        "Sent mail": {
+                            "special-use": ["\\Sent", "\\Drafts"],
+                            subscribed: false
+                        }
+                    }
+                },
+                "#news.":{
+                    type: "shared",
+                    separator: ".",
+                    folders: {
+                        "world":{}
+                    }
+                },
+                "#juke?":{
+                    type: "shared",
+                    separator: "?"
+                }
+            }
+        });
+
+        this.instanceId = ++instance;
+        this.server.listen(IMAP_PORT, (function(){
+            done();
+        }).bind(this));
+    },
+
+    tearDown: function(done){
+        this.server.close((function(){
+            done();
+        }).bind(this));
+    },
+
+    "LIST NORMAL": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST \"\" \"*\"",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.equal((resp.match(/^\* LIST\b/mg) || []).length, 3);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren) "/" "INBOX"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren) "/" "Test"\r\n') >= 0);
+            test.ok(resp.indexOf('\n* LIST (\\HasNoChildren) "/" "Sent mail"\r\n') >= 0);
+            test.ok(resp.indexOf("\nA3 OK") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST (SPECIAL-USE) \"\" \"*\"",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.ok(resp.indexOf("\nA3 BAD") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST RETURN (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST \"\" \"*\" RETURN (SPECIAL-USE)",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.ok(resp.indexOf("\nA3 BAD") >= 0);
+            test.done();
+        }).bind(this));
+    },
+
+    "LIST (SPECIAL-USE) RETURN (SPECIAL-USE)": function(test){
+        var cmds = ["A1 LOGIN testuser testpass",
+                "A2 CAPABILITY",
+                "A3 LIST (SPECIAL-USE) \"\" \"*\" RETURN (SPECIAL-USE)",
+                "ZZ LOGOUT"];
+
+        mockClient(IMAP_PORT, "localhost", cmds, false, (function(resp){
+            resp = resp.toString();
+            test.ok(resp.indexOf("\nA3 BAD") >= 0);
+            test.done();
+        }).bind(this));
+    }
+};
